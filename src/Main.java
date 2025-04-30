274806d46e95afbf6115347eda603ccd338aba31
BarboraElznic <barbora.elznic@gmail.com>
2025-04-30 12:42:18 +0200

    Změna v komentáři
    test blokového komentáře

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This is my first project");
-        //test komentáře
-        //
+        /* změna komentáře na blokový
+        * test komentáře
+        */
 
     }
 }
